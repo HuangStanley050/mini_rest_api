597a5c097f96df87ec15dee83bab6c4266b28918
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-09 04:50:47 +0000

    finishing signup route and controller plus validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,32 @@
 const express = require("express");
+const { body } = require("express-validator/check");
+const authController = require("../controllers/auth.js");
+const User = require("../models/user.js");
 const router = express.Router();
 
-router.put("/signup");
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("pleaes enter valid email")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.eject("Email already exist");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password")
+      .trim()
+      .isLength({ min: 5 }),
+    body("name")
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.signUp
+);
 
 module.exports = router;
