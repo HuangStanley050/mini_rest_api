ef0616d97269473d1b9c42ed3543194b21ebe6f0
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-09 05:19:02 +0000

    set up auth before implement JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.js");
 const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
 
 exports.signUp = (req, res, next) => {
   const errors = validationResult(req);
@@ -12,4 +13,52 @@ exports.signUp = (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        name: name
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({ message: "User created", userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error("An user couldn't be found with this email");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error("Wrong password");
+        error.statusCode = 401;
+        throw error;
+      }
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
